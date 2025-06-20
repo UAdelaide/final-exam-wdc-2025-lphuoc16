3d99396ff2462d24e2c70e46b9ded3701c66ef9a
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-06-20 05:31:38 +0000

    [automatic commit] 2025-06-20 05:31:38 UTC 203.221.169.149

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -51,7 +51,7 @@ router.get('/api/walkers/summary', async (req, res) => {
         u.username AS walker_username,
         COUNT(DISTINCT r.rating_id) AS total_ratings,
         ROUND(AVG(r.rating), 1) AS average_rating,
-        COUNT(DISTINCT CASE WHEN wr.status = 'completed' THEN wr.request_id END) AS completed_walks
+        COUNT
       FROM Users u
       LEFT JOIN WalkApplications AS wa
       ON u.user_id = wa.walker_id
