b9404911e8e85a29bbaf003b40b0825c00c9ebd8
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-06-20 04:53:43 +0000

    [automatic commit] 2025-06-20 04:53:43 UTC 203.221.169.149

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -34,7 +34,8 @@ router.get('/api/walkrequests/open', async (req, res) => {
         r.location AS location,
         u.username AS owner_name
       FROM WalkRequests r
-      JOIN Dogs d ON r.do
+      JOIN Dogs d ON r.dog_id = d.dog_id
+      JOIN Users u ON d.owner_id = 
     `);
   } catch (err) {
     res.status(500).json({ error: 'Internal server error' });
