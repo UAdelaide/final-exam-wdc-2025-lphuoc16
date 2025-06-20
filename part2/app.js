730f2af115d57b902e0911c7faa18975891ffefc
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-06-20 09:06:01 +0000

    [automatic commit] 2025-06-20 09:06:01 UTC 203.221.169.149

--- part2/app.js
+++ part2/app.js
@@ -6,6 +6,18 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
+
+app.use(express.json());
+app.use(session({
+  secret: 'a-very-secret-key',   // change to env var in production
+  resave: false,                 // don’t save session if unmodified
+  saveUninitialized: false,      // don’t create session until something stored
+  cookie: {
+    secure: false,              
+    maxAge: 1000 * 60 * 60 * 24
+  }
+}));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
