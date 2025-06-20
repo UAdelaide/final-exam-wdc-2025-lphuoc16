30e8539f6d09d8e87dc6941ca7b8eaf0d28549f9
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-06-20 03:47:35 +0000

    [automatic commit] 2025-06-20 03:47:35 UTC 203.221.169.149

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -11,6 +11,10 @@ router.get('/api/dogs', async (req, res) => {
   const [rows] = await db.query(`
     SELECT
       d.name AS dog_name,
+      d.size AS dog_size,
+      u.username AS owner_name
+    FROM
+      
   `)
 });
 module.exports = router;
