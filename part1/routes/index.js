1a289d0727f7b1c5910df80ab629a1cf50f55b58
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-06-20 04:59:05 +0000

    [automatic commit] 2025-06-20 04:59:05 UTC 203.221.169.149

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -49,6 +49,7 @@ router.get('/api/walkers/summary', async (req, res) => {
     const [rows] = await db.query(`
       SELECT
         u.username AS walker_username,
+        COUNT(r.rating_id) AS total_ratings,
         
     `);
   } catch (err) {
