065765fe6978ba1921a074fe0c2332b34bb1a44e
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-06-20 08:26:25 +0000

    [automatic commit] 2025-06-20 08:26:25 UTC 203.221.169.149

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -203,23 +203,23 @@ function login(){
     .catch((err) => {
         alert(err.message);
     });
-    // // Create AJAX Request
-    // var xmlhttp = new XMLHttpRequest();
+    // Create AJAX Request
+    var xmlhttp = new XMLHttpRequest();
 
-    // // Define function to run on response
-    // xmlhttp.onreadystatechange = function() {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         alert("Welcome "+this.responseText);
-    //     } else if (this.readyState == 4 && this.status >= 400) {
-    //         alert("Login failed");
-    //     }
-    // };
+    // Define function to run on response
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Welcome "+this.responseText);
+        } else if (this.readyState == 4 && this.status >= 400) {
+            alert("Login failed");
+        }
+    };
 
-    // // Open connection to server & send the post data using a POST request
-    // // We will cover POST requests in more detail in week 8
-    // xmlhttp.open("POST", "/users/login", true);
-    // xmlhttp.setRequestHeader("Content-type", "application/json");
-    // xmlhttp.send(JSON.stringify(user));
+    // Open connection to server & send the post data using a POST request
+    // We will cover POST requests in more detail in week 8
+    xmlhttp.open("POST", "/users/login", true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(JSON.stringify(user));
 
 }
 
