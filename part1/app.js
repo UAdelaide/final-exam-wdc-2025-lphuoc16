5a37bb4c739965c5867eb117a930dd6f5d5cf351
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-06-20 03:18:46 +0000

    [automatic commit] 2025-06-20 03:18:46 UTC 203.221.169.149

--- part1/app.js
+++ part1/app.js
@@ -53,14 +53,14 @@ let db;
         ('phuocwalker','phuoc@example.com',     'hashed200', 'walker')
     `);
     await db.execute(`
-    INSERT IGNORE INTO Dogs (owner_id, name, size)
-    VALUES
-      ((SELECT user_id FROM Users WHERE username='alice123'),'Max','medium'),
-      ((SELECT user_id FROM Users WHERE username='carol123'),'Bella','small'),
-      ((SELECT user_id FROM Users WHERE username='daveowner'),'Charlie','large'),
-      ((SELECT user_id FROM Users WHERE username='alice123'),'Daisy','medium'),
-      ((SELECT user_id FROM Users WHERE username='daveowner'),'Buddy','small')
-  `);
+        INSERT IGNORE INTO Dogs (owner_id, name, size)
+        VALUES
+        ((SELECT user_id FROM Users WHERE username='alice123'),'Max','medium'),
+        ((SELECT user_id FROM Users WHERE username='carol123'),'Bella','small'),
+        ((SELECT user_id FROM Users WHERE username='daveowner'),'Charlie','large'),
+        ((SELECT user_id FROM Users WHERE username='alice123'),'Daisy','medium'),
+        ((SELECT user_id FROM Users WHERE username='daveowner'),'Buddy','small')
+    `);
     }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
