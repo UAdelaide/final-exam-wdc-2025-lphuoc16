d88a69ffb2c5191ab43b863e3bfad1df13f08aaa
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-06-20 03:25:08 +0000

    [automatic commit] 2025-06-20 03:25:08 UTC 203.221.169.149

--- part1/app.js
+++ part1/app.js
@@ -53,7 +53,7 @@ let db;
         ('phuocwalker', 'phuoc@example.com', 'hashed200', 'walker')
     `);
     await db.execute(`
-        INSERT IGNORE INTO Dogs (owner_id, name, size)
+        INSERT INTO Dogs (owner_id, name, size)
         VALUES
         ((SELECT user_id FROM Users WHERE username='alice123'),'Max','medium'),
         ((SELECT user_id FROM Users WHERE username='carol123'),'Bella','small'),
@@ -62,7 +62,7 @@ let db;
         ((SELECT user_id FROM Users WHERE username='peterowner'),'Phuoc','small')
     `);
     await db.execute(`
-        INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
         VALUES
         ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
         ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
