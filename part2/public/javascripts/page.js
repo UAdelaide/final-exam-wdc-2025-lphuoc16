f28818bb1334e7418e9e024548e387b4dce1dfdd
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-06-20 11:55:01 +0000

    [automatic commit] 2025-06-20 11:55:01 UTC 203.221.169.149

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -253,5 +253,8 @@ async function loadDogTable() {
     try {
         const dogs = await fetch ('/api/users/dogs')
         .then((res) => res.json());
+        await Promise.all(dogs.map(async dog => {
+            
+        }))
     }
 }
