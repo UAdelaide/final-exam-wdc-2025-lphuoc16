8bfaa7ef76c97752823587ae982cabf971d60a59
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-06-20 11:51:19 +0000

    [automatic commit] 2025-06-20 11:51:19 UTC 203.221.169.149

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -82,4 +82,20 @@ router.get('/mydogs', async (req, res) => {
   }
 });
 
+router.get('/api/dogs', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+      SELECT
+        d.name AS dog_name,
+        d.size AS dog_size,
+        u.username AS owner_name
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id
+    `);
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 module.exports = router;
