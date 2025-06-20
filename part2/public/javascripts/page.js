ebd8466e804453a8368ca015f12742b3bf6466ca
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-06-20 08:46:13 +0000

    [automatic commit] 2025-06-20 08:46:13 UTC 203.221.169.149

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -181,7 +181,7 @@ function login(){
         password: document.getElementById('password').value
     };
 
-    fetch('/api/user/login', {
+    fetch('/user/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(user)
